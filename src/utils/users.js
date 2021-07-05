c751b6d24e8c98a7084cfbe9301670f288e785ee
miguelDalberto <migueldalberto.acc@gmail.com>
2021-07-05 19:10:05 -0300

    add functions to track users and their rooms

---
+++ src/utils/users.js
@@ -0,0 +1,48 @@
+const users = []
+
+const addUser = ({ id, nickname, room }) => {
+  // clean data
+  nickname = nickname.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  // validate data
+  // check if data exists
+  if(!nickname || !room) {
+    return { error: 'Nickname and room are required.' }
+  }
+  // check if nicknames is taken
+  const isNicknameTaken = users.find((user) => user.room === room && user.nickname === nickname)
+  if(isNicknameTaken) {
+    return { error: 'Nickname is taken. Try another one.' }
+  }
+
+  // store user
+  const user = {id, nickname, room}
+  users.push(user)
+  return { user }
+}
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+const getUser = (id) => {
+  const index = users.findIndex((user) => user.id === id)
+
+  if(index !== -1) {
+    return users[index]
+  }
+}
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
