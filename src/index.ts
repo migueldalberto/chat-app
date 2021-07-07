c9639af2a50359f01b46ad38818eae17d0c8081f
miguelDalberto <migueldalberto.acc@gmail.com>
2021-07-07 16:38:07 -0300

    add tracking of the active rooms

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const { Server } = require('socket.io')
 
 import { createMessage } from './utils/messages'
 import { User, UserList } from './utils/users'
+import { Room } from './utils/rooms'
 
 const app = express()
 const server = http.createServer(app)
@@ -14,16 +15,27 @@ const io = new Server(server)
 // serving public directory
 app.use(express.static(path.join(__dirname, '../public')))
 
-// start tracking users
+// start tracking users and rooms
 const usersOnline = new UserList()
+const activeRooms: Room[] = []
 
 io.on('connection', (socket) => {
+  socket.emit('active rooms', activeRooms)
+
   socket.on('disconnect', () => {
     const user = usersOnline.removeUser(socket.id)
     if(user) {
-      io.to(user.getRoom()).emit('message', createMessage('SYSTEM', `${user.getNickname()} has left.`))
+      const room = user.getRoom()
+      io.to(room).emit('message', createMessage('SYSTEM', `${user.getNickname()} has left.`))
+      const index = activeRooms.findIndex((r) => r.getRoomName() === room)
 
-      io.to(user.getRoom()).emit('room data', {
+      if(activeRooms[index].total() === 1) {
+        activeRooms.splice(index, 1)
+      } else {
+        activeRooms[index].removeUser(socket.id) 
+      }
+
+      io.to(room).emit('room data', {
         room: user.getRoom(),
         users: usersOnline.getUsersInRoom(user.getRoom())
       })
@@ -43,7 +55,16 @@ io.on('connection', (socket) => {
     const user = new User(socket.id, options.nickname, options.room)
     usersOnline.addUser(user)
 
-    socket.join(user.getRoom())
+    const room = new Room(user.getRoom())
+    socket.join(room.getRoomName())
+
+    const index = activeRooms.findIndex((r) => r.getRoomName() === room.getRoomName())
+    if (index !== -1) {
+      activeRooms[index].addUser(user)
+    } else {
+      room.addUser(user)
+      activeRooms.push(room)
+    }
 
     socket.broadcast.to(user.getRoom()).emit('message', createMessage('SYSTEM', `${user.getNickname()} has joined.`))
     io.to(user.getRoom()).emit('room data', {
---
+++ src/utils/rooms.ts
@@ -0,0 +1,19 @@
+import { User, UserList } from './users'
+
+export class Room extends UserList {
+  private name: string;
+
+  constructor(name: string) {
+    super()
+
+    this.name = name;
+  }
+
+  /**
+   * getRoomName
+   * @returns room name
+   */
+  public getRoomName(): string {
+    return this.name
+  }
+}
